db5891326ff16b88036684042c2e0c2e2f9c48d4
GrantSteinfeld <grant.steinfeld@ibm.com>
2019-02-01 17:43:54 -0500

    merged Horea's changes from master

--- src/controllers/shipper.controller.ts
+++ src/controllers/shipper.controller.ts
@@ -1,92 +1,123 @@
-/* tslint:disable:no-any */
-import {operation, param, requestBody} from '@loopback/rest';
-import {Shipper} from '../models/shipper.model';
+import { Request, RestBindings, get, post, requestBody, ResponseObject, param } from '@loopback/rest';
+import { inject } from '@loopback/context';
+import { Shipper } from '../models';
+
+var BlockchainClient = require('../blockchainClient.js');
+//import { BlockchainClient } from '../blockchainClient';
+
+var blockchainClient = new BlockchainClient();
+
 
 /**
- * The controller class is generated from OpenAPI spec with operations tagged
- * by Shipper
- * A participant named Shipper
+ * OpenAPI response for Shipper()
+ */
+const Shipper_RESPONSE: ResponseObject = {
+  description: 'Pour Shipper Response',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        properties: {
+          greeting: { type: 'string' },
+          date: { type: 'string' },
+          url: { type: 'string' },
+          headers: {
+            type: 'object',
+            properties: {
+              'Content-Type': { type: 'string' },
+            },
+            additionalProperties: true,
+          },
+        },
+      },
+    },
+  },
+};
+
+
+/**
+ * A simple controller to bounce back http requests
  */
 export class ShipperController {
-  constructor() {}
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) { }
 
-  /**
-   * 
-   * 
 
-   * @param requestBody Model instance data
-   * @returns Request was successful
-   */
-  @operation('post', '/Shipper')
-  async shipperCreate(@requestBody() requestBody: Shipper): Promise<Shipper> {
-    throw new Error('Not implemented');
+  @get('/Shipper/{shipperId}', {
+    responses: {
+      '200': {
+        description: 'Shipper model instance',
+        content: { 'application/json': { schema: { 'x-ts-type': Shipper } } },
+      },
+    },
+  })
+  async findById(@param.path.string('shipperId') shipit: string): Promise<Shipper> {
+
+
+    let networkObj = await blockchainClient.connectToNetwork();
+    let dataForQuery = {
+      function: 'query',
+      id: shipit,
+      contract: networkObj.contract,
+      network: networkObj.network
+    };
+    let rez = await blockchainClient.queryByKey(dataForQuery);
+    console.log("rez :: <Buffer> tbd how to work this .... ");
+
+    console.log(rez);
+    console.log(JSON.parse(rez.toString()));
+    let x = new Shipper({ id: 33, organization: 'ace', address: '43 Palm Lane', shipperId: 'shippersId' });
+    return x;
+    //return await this.ShipperRepository.findById(id);
+
   }
 
-  /**
-   * 
-   * 
-
-   * @param filter Filter defining fields, where, include, order, offset, and limit - must be a JSON-encoded string ({"something":"value"})
-   * @returns Request was successful
-   */
-  @operation('get', '/Shipper')
-  async shipperFind(@param({name: 'filter', in: 'query'}) filter: string): Promise<Shipper[]> {
-    throw new Error('Not implemented');
+  // Map to `GET /Shipper`
+  @get('/Shipper', {
+    responses: {
+      '200': Shipper_RESPONSE,
+    },
+  })
+  Shipper(): object {
+    // Reply with a greeting, the current time, the url, and request headers
+    return {
+      greeting: 'found Shipper',
+      date: new Date(),
+      url: this.req.url,
+      headers: Object.assign({}, this.req.headers),
+    };
   }
 
-  /**
-   * 
-   * 
+  // Map to `POST`
+  @post('/Shipper', {
+    responses: {
+      '200': {
+        description: 'Shipper model instance',
+        content: { 'application/json': { schema: { 'x-ts-type': Shipper } } },
+      },
+    },
+  })
+  async create(@requestBody() shipper: Shipper): Promise<Shipper> {
+    //example of how to submit args to transaction - this can be changed
+    //  async addMember(ctx, id, organization, address, memberType) {
 
-   * @param id Model id
-   * @returns Request was successful
-   */
-  @operation('head', '/Shipper/{id}')
-  async shipperExists(@param({name: 'id', in: 'path'}) id: string): Promise<{
-  exists?: boolean;
-}> {
-    throw new Error('Not implemented');
+    console.log('shipper: ')
+    console.log(shipper)
+    let networkObj = await blockchainClient.connectToNetwork();
+    let dataForAddMember = {
+      function: 'addMember',
+      id: shipper.shipperId,
+      organization: shipper.organization,
+      address: shipper.address,
+      memberType: 'shipper',
+      contract: networkObj.contract
+    };
+    var result = blockchainClient.submitTransaction(dataForAddMember);
+    console.info(result);
+
+
+    return shipper;
+    //return await this.ShipperRepository.create(Shipper);
   }
-
-  /**
-   * 
-   * 
-
-   * @param id Model id
-   * @param filter Filter defining fields and include - must be a JSON-encoded string ({"something":"value"})
-   * @returns Request was successful
-   */
-  @operation('get', '/Shipper/{id}')
-  async shipperFindById(@param({name: 'id', in: 'path'}) id: string, @param({name: 'filter', in: 'query'}) filter: string): Promise<Shipper> {
-    throw new Error('Not implemented');
-  }
-
-  /**
-   * 
-   * 
-
-   * @param requestBody Model instance data
-   * @param id Model id
-   * @returns Request was successful
-   */
-  @operation('put', '/Shipper/{id}')
-  async shipperReplaceById(@requestBody() requestBody: Shipper, @param({name: 'id', in: 'path'}) id: string): Promise<Shipper> {
-    throw new Error('Not implemented');
-  }
-
-  /**
-   * 
-   * 
-
-   * @param id Model id
-   * @returns Request was successful
-   */
-  @operation('delete', '/Shipper/{id}')
-  async shipperDeleteById(@param({name: 'id', in: 'path'}) id: string): Promise<{
-  
-}> {
-    throw new Error('Not implemented');
-  }
-
 }
+
 
